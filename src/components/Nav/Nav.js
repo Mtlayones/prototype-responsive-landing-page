b3fc916cba86a17abf11d8e0d5d7e3ca454ae5d0
mtlayones <mtlayones@up.edu.ph>
2021-06-20 18:53:03 +0800

    finish landing page

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,68 @@
+import styles from './Nav.module.css';
+import Image from 'next/image';
+import logo from '../../assets/logo.svg';
+import { useState, useEffect, useRef } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+const useClickOutside = (ref, callback) => {
+    const handleClick = (e) => {
+        if (ref.current && !ref.current.contains(e.target)) {
+            callback();
+        }
+    };
+    const handleResize = (e) => {
+        callback();
+    };
+    useEffect(() => {
+        document.addEventListener('click', handleClick);
+        window.addEventListener('resize', handleResize);
+        return () => {
+            document.removeEventListener('click', handleClick);
+            window.removeEventListener('resize', handleResize);
+        };
+    });
+};
+
+const Nav = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const componentRef = useRef();
+    useClickOutside(componentRef, () => {
+        if (toggleMenu) {
+            setToggleMenu(false);
+        }
+    });
+    return (
+        <nav className={styles.nav_custom}>
+            <div className={styles.nav_items_noncontractable}>
+                <button
+                    className={styles.button_menu}
+                    onClick={() => setToggleMenu(!toggleMenu)}>
+                    <AiOutlineMenu />
+                </button>
+                <button className={styles.button_logo}>
+                    <Image src={logo} alt='logo-image' />
+                </button>
+            </div>
+            <ul
+                className={
+                    toggleMenu
+                        ? styles.nav_items_contractable_true
+                        : styles.nav_items_contractable
+                }
+                ref={componentRef}>
+                <li className="w-full flex justify-center"><button className={styles.button_tertiary}>HOME</button></li>
+                <li className="w-full flex justify-center"><button className={styles.button_tertiary}>ABOUT US</button></li>
+                <li className="w-full flex justify-center"><button className={styles.button_tertiary}>CONTACT</button></li>
+                <li className="mr-0 md:mr-2 w-full flex justify-center"><button className={styles.button_tertiary}>CART</button></li>
+                <li className="w-full flex justify-center">
+                    <button className={styles.button_primary}>Sign In</button>
+                </li>
+                <li className="w-full flex justify-center">
+                    <button className={styles.button_secondary}>Sign Up</button>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: '20rem',
+      md: '42rem',
+      lg: '66rem',
+      xl: '82rem',
+      max: '99rem',
+    },
+    extend: {
+    },
   },
   variants: {
     extend: {},
